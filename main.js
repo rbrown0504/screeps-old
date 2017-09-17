8d3cb776dde9d81fa6f5b1daedc48727e855da27
Robert Brown <robert@Roberts-iMac.home>
2017-09-17 12:25:39 -0700

    Log Upgraders
    Added log for upgraders

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ module.exports.loop = function () {
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    console.log('Upgraders: ' + upgraders.length);
+
     if(harvesters.length < 2) {
         var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
