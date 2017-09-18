e6b4bb7873aa7df9384a699a2eb2bb4753decf05
Robert Brown <robert@Roberts-iMac.home>
2017-09-17 18:53:16 -0700

    Find nearest source
    For upgraders and builders.

--- role.builder.js
+++ role.builder.js
@@ -21,10 +21,11 @@ var roleBuilder = {
             }
 	    }
 	    else {
-	        var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
+	    	//FIND NEAREST SOURCE AND GET STUFF
+	    	var source = creep.pos.findClosestByRange(FIND_SOURCES);
+	    	creep.memory.sourceId = source.id;
+	    	var source = Game.getObjectById(creep.memory.sourceId);
+			creep.moveTo(source,{visualizePathStyle: {stroke: '#ffaa00'}}); // OK
 	    }
 	}
 };
