eeb0a43422e12380ac14eab8fd4b0fc0f20f2e8c
Robert Brown <robert@Roberts-iMac.home>
2017-09-21 17:11:21 -0700

    Retired Repairer

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
-var roleRepairer = require('role.repairer');
 
 //load room utilities
 var roomMain = require('roomMain');
@@ -147,9 +146,6 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'builder') {
             roleBuilder.run(creep);
-        }
-        if(creep.memory.role == 'repairer') {
-            roleRepairer.run(creep);
         }
     }
     //console.log(harvestersAssigned.size);
