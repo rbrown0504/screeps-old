f35e1afcf5a0db54b662a9bf79ce03c54c58e001
Robert Brown <robert@Roberts-iMac.home>
2017-09-21 17:02:41 -0700

    Updated to new createCreep structure
    Also added reason code in console log for creep create failure.

--- creepHandler.js
+++ creepHandler.js
@@ -61,19 +61,73 @@ creepHandler.prototype.new = function(creepType, spawn) {
 
 	switch(creepType) {
 		case 'roleMiner':
-		/*case 'harvester':
-			if(level <= 1) {
-				abilities = [WORK, CARRY, MOVE];
-			}
-		case 'builder':
-			if(level <= 1) {
-				abilities = [WORK, CARRY, MOVE];
-			}
+			abilities = [WORK, CARRY, MOVE];
+			break;
 		case 'upgrader':
 			if(level <= 1) {
 				abilities = [WORK, CARRY, MOVE];
-			}*/
+			} else
+			if(level <= 2) {
+				abilities = [WORK, WORK, CARRY, MOVE];
+			} else
+			if(level <= 3) {
+				abilities = [WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 4) {
+				abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 5) {
+				abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE];
+			} else
+			if(level <= 6) {
+				abilities = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 7) {
+				abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 8) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 9) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE];
+			} else
+			if(level >= 10) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE];
+			}
+		break;
 		case 'roleBuilder':
+			if(level <= 1) {
+				abilities = [WORK, CARRY, MOVE];
+			} else
+			if(level <= 2) {
+				abilities = [WORK, WORK, CARRY, MOVE];
+			} else
+			if(level <= 3) {
+				abilities = [WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 4) {
+				abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 5) {
+				abilities = [WORK, WORK, WORK, CARRY, MOVE, MOVE, MOVE];
+			} else
+			if(level <= 6) {
+				abilities = [WORK, WORK, WORK, WORK, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 7) {
+				abilities = [WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 8) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE];
+			} else
+			if(level <= 9) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE];
+			} else
+			if(level >= 10) {
+				abilities = [WORK, WORK, WORK, WORK, WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE];
+			}
+		break;
+		case 'builder':
 			if(level <= 1) {
 				abilities = [WORK, CARRY, MOVE];
 			} else
@@ -205,7 +259,8 @@ creepHandler.prototype.new = function(creepType, spawn) {
 		}
 	);
 	if(canBuild !== 0) {
-		console.log('Can not build creep: ' + creepType + ' @ ' + level);
+		console.log('Can not build creep: ' + creepType + ' @ ' + level + ' | Reason: ' + canBuild ) ;
+		//console.log(this.creepUtility.getSpawnFailure(canBuild));
 		return;
 	}
 
